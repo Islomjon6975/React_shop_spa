e0e9e765ee1f81f34d1f3a5c702854ea2e013d1a
islomjon6975 <islomjon0919@mail.ru>
2022-03-16 16:37:44 +0500

    finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,20 +3,35 @@ import {getAllCategories} from '../api';
 import {Loader} from '../components/Loader';
 import { CategoryList } from "../components/CategoryList";
 import Search from "../components/Search";
+import { useLocation, useHistory } from "react-router-dom";
 
 
 export default function Home () {
     const [catalog, setCatalog] = useState([]);
     const [filteredCatalog, setFilteredCatalog] = useState([]);
 
+    const {pathname, search} = useLocation();
+    const {push} = useHistory();
+    
+
     const handleSearch = (str) => {
-        setFilteredCatalog(catalog.filter(item => item.strCategory.toLowerCase().includes(str.toLowerCase())))
+        setFilteredCatalog(catalog.filter((item) => item.strCategory.toLowerCase().includes(str.toLowerCase())))
+        push({
+            pathname,
+            search: `?search=${str}`
+        })
     }
 
     useEffect(()=>{
         getAllCategories().then(data => {
             setCatalog(data.categories);
-            setFilteredCatalog(data.categories);
+            setFilteredCatalog(search ? 
+                data.categories.filter(item=>
+                    item.strCategory
+                    .toLowerCase()
+                    .includes(search.split("=")[1].toLowerCase())
+                    ) : data.categories
+                    );
         });
     });
 
